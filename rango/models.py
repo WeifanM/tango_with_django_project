8bae1f13a626a3d9327b6f71400907e3d253ec68
Weifan Ma <2921952m@student.gla.ac.uk>
2024-02-13 23:27:00 +0000

    Feb 13 about chapter 8

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,18 @@
 #from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create your models here.
 # 在 'rango' 应用的 models.py 文件中
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)  # 新添加的 likes 字段
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     class Meta:
         verbose_name_plural = 'Categories'
 
